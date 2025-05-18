55af92d6674a3b77dedc71d30115470969a9259d
Xiaowen Li <lxw243@gmail.com>
2025-05-19 11:56:43 +1200

    Update args.py

--- src/utils/args.py
+++ src/utils/args.py
@@ -1,24 +1,43 @@
-import torch
 import argparse
-
-
-# ----- Parser -----
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 def parser():
     PARSER = argparse.ArgumentParser(description='Training parameters.')
 
-    PARSER.add_argument('--dataset', default='ROSMAP', type=str,
-                        choices=['ROSMAP', 'BRCA'],
-                        help='Dataset.')
+    # Model choice
+    PARSER.add_argument('--model', default='GeneGroupModel_two_layer', type=str,
+                        help='Model architecture to use.')
     
-    PARSER.add_argument('--model', default='NN_sum', type=str,
-                        choices=['NN_concat','CrossModal_NN_concat'], 
-                        help='Model.')
-
+    # method name
+    PARSER.add_argument('--method_name', default='MO_MKL_GL', type=str)
+    
+    # task type
+    PARSER.add_argument('--task_type', default='binary_class', type=str,
+                       choices=['regression', 'binary_class','multi_class'])
+    
+    # num_classese
+    PARSER.add_argument('--num_classes', default=1, type=int)
+    
+    
+    # dataset name
+    PARSER.add_argument('--dataset', default='KEGG', type=str)
+    
+    # unique trials
+    PARSER.add_argument('--trials', default=30, type=int)
+    
+    # Device choice
     PARSER.add_argument('--device', default='cpu', type=str,
-                        choices=['cpu', 'cuda:0', 'cuda:1', 'cuda:3', 'cuda:2'],
+                        choices=['cpu', 'cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'],
                         help='Device to run the experiment.')
 
+    # Regularization parameters
+    PARSER.add_argument('--penaltytype', default='group_lasso', type=str,
+                        choices=['group_lasso', 'sparse_group_lasso', 'none','adaptive_sparse_group_lasso','adaptive_group_lasso'],
+                        help='Type of regularization: group_lasso,sparse_group_lasso, none, adaptive_sparse_group_lasso, adaptive_group_lasso')
+    
+    # Experiment name
     PARSER.add_argument('--exp_name', default='test', type=str)
     
     ARGS = PARSER.parse_args()
@@ -26,9 +45,8 @@ def parser():
     if ARGS.device is None:
         ARGS.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    #print_args(ARGS)
-
     return ARGS
+
 
 args = parser()
 

4defa2e2b3c1f74c4f5f7af8a39176ba88532e19
Xiaowen Li <lxw243@gmail.com>
2025-05-19 11:52:28 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,123 +1,200 @@
-from torch.utils.data import SubsetRandomSampler, DataLoader
-from src.utils import args
-from src.models import load_model
-from src.utils.load_data import load_data
-from src.utils import *
-from src.opt.training_pipeline import train_model, train_step, valid_step,train_step_with_group_lasso
+from torch.utils.data import SubsetRandomSampler, DataLoader,TensorDataset
+from GResNNSGL.utils import args
+from GResNNSGL.models import load_model
+from GResNNSGL.utils.load_data import load_data
+from GResNNSGL.utils.utils import compute_adaptive_weights
+from GResNNSGL.utils import *
+from GResNNSGL.opt.training_pipeline import train, validate, objective
 import optuna
+import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
+import logging
+import sys, random, os, math, string
+import builtins
+from sklearn.metrics import mean_squared_error
+from scipy.stats import pearsonr
+import csv
+from itertools import product
+from torch.optim import Optimizer
+from torch.optim import Adam
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
+# costumise with your values
+job_id = os.environ['SLURM_JOB_ID']
+job_name = os.environ['SLURM_JOB_NAME']
+method = args.method_name
+dataset = args.dataset
+
+# change with your path
+group_weight_path = '/nesi/project/uoa03056/MultiOmics/ADNI/GWAS/GWAS_union_ad_genes.csv'
+
+
+def compute_group_importance(best_model, test_loader, criterion, gene_groups, args):
+    """ Computes group importance using permutation-based method """
+    original_loss = validate(best_model, test_loader, criterion, args)["loss"]
+    group_importance = {}
+
+    for group_idx, group in enumerate(gene_groups):
+        permuted_X = test_loader.dataset.tensors[0].clone()  # Copy test data
+        permuted_X[:, group] = permuted_X[torch.randperm(permuted_X.size(0)), group]  # Shuffle sample order in the group
+
+        permuted_dataset = TensorDataset(permuted_X, test_loader.dataset.tensors[1])
+        permuted_loader = DataLoader(permuted_dataset, batch_size=len(permuted_dataset), shuffle=False)
+
+        permuted_loss = validate(best_model, permuted_loader, criterion, args)["loss"]
+        group_importance[group_idx] = original_loss - permuted_loss  # smaller < 0 = more important
+
+    return group_importance
 
 
 def single_run(seed):
-    fix_random_seed(seed)
+    
+    train_stats = None  # Initialize variable to prevent referencing before assignment
+    test_stats = None
+    
+    try:
+        fix_random_seed(seed)
 
-    objective = lambda trial: train_model(trial, seed)
+        # Define the objective function for Optuna
+        objective_fn = lambda trial: objective(trial, seed)
 
-    # RUN loop for hyper-parameter optimization
-    sampler = optuna.samplers.TPESampler(seed=1)
-    study = optuna.create_study(direction="maximize", sampler=sampler) # accuracy as metric to optimize parameters
-    unique_trials = 20
-    while unique_trials > len(set(str(t.params) for t in study.trials)):
-        study.optimize(objective, n_trials=1)
+        # Create an Optuna study
+        sampler = optuna.samplers.TPESampler(seed=seed)
+        study = optuna.create_study(direction="minimize", sampler=sampler)
 
-    # best set of hyperparameter selected
-    best_params = study.best_trial.params
+        unique_trials = args.trials
+        while unique_trials > len(set(str(t.params) for t in study.trials)):
+            study.optimize(objective_fn, n_trials=1, n_jobs=-1)
 
-    kwargs = {} if args.device=='cpu' else {'num_workers': 2, 'pin_memory': True}
-    loader_kwargs = {**kwargs}
+        # Select the best hyperparameters
+        best_params = study.best_trial.params
+        max_best_epoch = best_epoch = study.best_trial.user_attrs.get("max_best_epoch", None)
 
-    data_1, data_2, data_3 = load_data(seed=seed, kernel_par = [best_params['kernel_par_1'], best_params['kernel_par_2'], best_params['kernel_par_3']],
-                                        n_principal= best_params['n_principal'])
-    #print(data_1[0][0])
+        # Data loading
+        kwargs = {} if args.device == 'cpu' else {'num_workers': 2, 'pin_memory': True}
+        loader_kwargs = {**kwargs}
 
-    # Create indices for the dataset that you want to shuffle
-    fix_random_seed(seed)
-    indices_view1 = np.arange(len(data_1[0]))
-    np.random.shuffle(indices_view1)
+        # Load data
+        fix_random_seed(seed)
+        X_tr, X_te, y_tr,y_te, gene_groups = load_data(seed)
 
-    # Create SubsetRandomSampler using the shuffled indices
-    sampler_view1 = SubsetRandomSampler(indices_view1)
+        # Create DataLoaders
+        train_loader = DataLoader(TensorDataset(X_tr, y_tr), batch_size=len(X_tr), shuffle=True, **loader_kwargs)
+        test_loader = DataLoader(TensorDataset(X_te, y_te), batch_size=len(X_te), shuffle=False, **loader_kwargs)
 
-    # omic1
-    train_loader_1 = DataLoader(data_1[0], **loader_kwargs, sampler=sampler_view1, batch_size=best_params['batch_size'])
-    test_loader_1  = DataLoader(data_1[1], **loader_kwargs, batch_size=best_params['batch_size'])  
-    # omic2
-    train_loader_2 = DataLoader(data_2[0], **loader_kwargs, sampler=sampler_view1, batch_size=best_params['batch_size'])
-    test_loader_2  = DataLoader(data_2[1], **loader_kwargs, batch_size=best_params['batch_size'])  
-    # omic3
-    train_loader_3 = DataLoader(data_3[0], **loader_kwargs, sampler=sampler_view1, batch_size=best_params['batch_size'])
-    test_loader_3  = DataLoader(data_3[1], **loader_kwargs, batch_size=best_params['batch_size']) 
+        # Initialize the best model with optimal parameters
+        best_model = load_model(best_params, gene_groups, args.model).to(args.device)
 
-    # loader for each omic, train and validation
-    train_loader =  {'omic1': train_loader_1, 'omic2': train_loader_2, 'omic3': train_loader_3}
-    test_loader =  {'omic1': test_loader_1, 'omic2': test_loader_2, 'omic3': test_loader_3}
+        # Define loss function
+        if args.task_type == 'regression':
+            criterion = nn.MSELoss()  
+        elif args.task_type == 'binary_class':
+            criterion = nn.BCEWithLogitsLoss()  
+        else:  
+            criterion = nn.CrossEntropyLoss() 
+            
+        optimizer = optim.Adam(best_model.parameters(), lr=best_params['learning_rate'], weight_decay=best_params['weight_decay'])
 
-    input_size_1, input_size_2, input_size_3 =  data_1[0][0][0].shape[0], data_2[0][0][0].shape[0], data_3[0][0][0].shape[0]
+        # Early stopping parameters
+        max_epoch = max_best_epoch
+        best_val_loss = float('inf')
+        best_epoch = 0
+        patience = 10
+        patience_counter = 0
+            
+        for epoch in range(max_epoch):
+            train_stats = train(best_model, train_loader, optimizer, criterion, best_params['lambda_lasso'], best_params['lambda_group'], gene_groups, args,adaptive_weights = None)
+               
+        # Evaluate the model on the test set
+        test_stats = validate(best_model, test_loader, criterion,args)
 
-    # initialize besat model
-    best_model = load_model(best_params, input_size_1, input_size_2, input_size_3, args.model).to(args.device)
-
-    # loss function
-    criterion = nn.CrossEntropyLoss()
-
-    # define optimizer
-    if best_params['optimizer'] in  ["Adagrad", "Adam", "AdamW", "SGD"]:
-        best_optimizer = getattr(optim, best_params['optimizer'])(best_model.parameters(), lr= best_params['learning_rate'], weight_decay=best_params['weight_decay'])
-    else:
-        best_optimizer = getattr(optim, best_params['optimizer'])(best_model.parameters(), lr= best_params['learning_rate'])
-
-    for epoch in range(best_params['epochs']):
-        train_stats = train_step_with_group_lasso(best_model, criterion, best_optimizer, train_loader, lambda_=best_params['lambda_group_lasso'])
-        print(f"Epoch {epoch + 1}, Group Norms: {train_stats['group_norms']}")
+        print(f'Best hyperparameters for this split: {best_params}')
+        print(f'Train MSE: {train_stats["loss"]}')
+        print(f'Test MSE: {test_stats["loss"]}')
         
-    test_stats = valid_step(best_model, criterion, test_loader)
+        if args.task_type == 'regression':
+            print(f'Train Corr: {train_stats["correlation"]}')
+            print(f'Test Corr: {test_stats["correlation"]}')
+        else:
+            print(f'Test Accuracy: {test_stats["accuracy"]}')
+            print(f'Test F1 Score: {test_stats["f1"]}')
+            print(f'Test AUC-ROC: {test_stats["auc_roc"]}')
+        
+        group_importance = compute_group_importance(best_model, test_loader, criterion, gene_groups, args)
+     
+        with open(f'/nesi/project/uoa03056/MO_MKL_GL/results/results_{method}_{dataset}.csv', mode='a', newline='') as file:
+        #with open(f'/nesi/project/uoa03056/MO_MKL_GL/results/WM/results_GeneResModel_GWAS.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            if file.tell() == 0:
+                if args.task_type == 'regression':
+                    writer.writerow(['Model', 'Run', 'MSE', 'Correlation', 'Best_Epoch', 'dropout1', 'learning_rate', 'weight_decay', 'lambda_group', 'lambda_lasso', 'hidden1', 'hidden2', 'hidden3'])
+                else:
+                    writer.writerow(['Model', 'Run', 'Loss', 'Accuracy', 'F1', 'AUC-ROC', 'Best_Epoch', 'dropout1', 'learning_rate', 'weight_decay', 'lambda_group', 'lambda_lasso', 'hidden1', 'hidden2', 'hidden3'])
+            if args.task_type == 'regression':
+                row = [
+                    method, seed, test_stats["loss"], test_stats["correlation"], max_epoch, best_params['dropout'], best_params['learning_rate'], best_params['weight_decay'], best_params['lambda_group'], best_params['lambda_lasso'], best_params['hidden1_size'], best_params['hidden2_size'], best_params['hidden3_size']]
 
-    print(f'Best hyperparameters for this split: {best_params}')
-    print(f'Train Accuracy: {train_stats["accuracy"]}')
-    print(f'Test Accuracy: {test_stats["accuracy"]}')
-    print()
-    if args.dataset == 'ROSMAP':
-        print(f'Test AUC: {test_stats["auc"]}')
-        print(f'Test F1: {test_stats["f1"]}')
-        return test_stats["accuracy"], test_stats["auc"], test_stats["f1"]
+            else:
+                row = [
+                    method, seed, test_stats["loss"],
+                    test_stats["accuracy"], test_stats["f1"], test_stats["auc_roc"],
+                    max_epoch, best_params['dropout'], best_params['learning_rate'], best_params['weight_decay'], best_params['lambda_group'], best_params['lambda_lasso'], best_params['hidden1_size'], best_params['hidden2_size'], best_params['hidden3_size']
+                ]
+            writer.writerow(row)
+    
+        # Save the model
+        save_dir = f'/nesi/nobackup/uoa03056/MO_MKL_GL/save_models/{method}_{dataset}'
+        os.makedirs(save_dir, exist_ok=True)
+        model_save_path = os.path.join(save_dir, f"{method}_{dataset}_best_model_seed_{seed}.pth")
+        torch.save(best_model, model_save_path)
+        
+        #save final selected groups and all weights for each gene groups.
+        # Save the selected features and weights to a CSV file
+        base_path_feature = f'/nesi/project/uoa03056/MO_MKL_GL/selected_features/' 
+        save_path_feature = os.path.join(base_path_feature, f"{method}_{dataset}")
+        os.makedirs(save_path_feature, exist_ok=True)
+        
+        weight_file_path = os.path.join(save_path_feature, f"{method}_{dataset}_weights_seed_{seed}.csv")
+        feature_file_path = os.path.join(save_path_feature, f"{method}_{dataset}_select_feature_seed_{seed}.csv")
+        best_model.save_weights_and_groups(weight_file_path, feature_file_path)
+        
+        importance_file_path = os.path.join(save_path_feature, f"{method}_{dataset}_group_importance_seed_{seed}.csv")
+        with open(importance_file_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Group", "Importance"])
+            for group, importance in group_importance.items():
+                writer.writerow([group, importance])
 
-    else:
-        print(f'Test F1_macro: {test_stats["f1"][0]}')
-        print(f'Test F1_weighted: {test_stats["f1"][1]}')
-        return test_stats["accuracy"], test_stats["f1"][0], test_stats['f1'][1]
+                
 
-
+        return test_stats["loss"], test_stats["correlation"] if args.task_type == 'regression' else (test_stats["accuracy"], test_stats["f1"], test_stats["auc_roc"])
+    
+    except Exception as e:
+        print(f"Error during single_run with seed {seed}: {e}")
+        return float("inf"), 0.0
 
 def main():
-    accuracies = []
-    aucs = []
-    f1s = []
-    f1_mcs = []
-    f1_wes = []
-    if args.dataset=='ROSMAP':
-        for seed in range(5):
-            accuracy, auc, f1 = single_run(seed=seed)
-            accuracies.append(accuracy)
-            aucs.append(auc)
-            f1s.append(f1)
-
-        print(f'Average TEST ACC over 5 runs: {np.mean(accuracies)}, STD {np.std(accuracies)}')
-        print(f'Average TEST AUC over 5 runs: {np.mean(aucs)}, STD {np.std(aucs)}')
-        print(f'Average TEST F1 over 5 runs: {np.mean(f1s)}, STD {np.std(f1s)}')
-    else:
-        for seed in range(5):
-            accuracy, f1_mc, f1_we = single_run(seed=seed)
-            accuracies.append(accuracy)
-            f1_mcs.append(f1_mc)
-            f1_wes.append(f1_we)
-
-        print(f'Average TEST ACC over 5 runs: {np.mean(accuracies)}, STD {np.std(accuracies)}')
-        print(f'Average TEST F1 macro over 5 runs: {np.mean(f1_mcs)}, STD {np.std(f1_mcs)}')
-        print(f'Average TEST F1 Weightew 5 runs: {np.mean(f1_wes)}, STD {np.std(f1_wes)}')
-
+    results = []
+    run = 1
+    seed = int(os.getenv('SLURM_ARRAY_TASK_ID', 0))
+    
+    for r in range(1, run + 1):
+        try:
+            result = single_run(seed=seed)
+            results.append(result)
+            print(f"Run {r}: {result}")
+        except Exception as e:
+            print(f"Run {r}: Failed with seed {seed}, Error: {e}")
+            continue  # Skip the failed run
+  
         
+if __name__ == "__main__":
+    main()
+
     
 
 if __name__ == "__main__":

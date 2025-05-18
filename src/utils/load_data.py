7b9f92209b9d0e67b6a0edef709bfafecab8aa88
Xiaowen Li <lxw243@gmail.com>
2025-05-19 11:57:34 +1200

    Update load_data.py

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -1,97 +1,88 @@
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,TensorDataset,DataLoader
 import pandas as pd
 from .args import args
 from sklearn.model_selection import train_test_split
-from sklearn.decomposition import KernelPCA
 from src.utils import *
-
-# ----- Dataset -----
-class SingleOmicDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
-
-    def __len__(self):
-        return len(self.data[0])
-
-    def __getitem__(self, index):
-        x = self.data[0][index]
-        y = self.data[1][index]
-
-        return x, y
+import os
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
 
 
-def import_dataset(dataset=args.dataset):
-    path = "/workspaces/nn_integration/data/" + dataset
+def load_data(seed, dataset=args.dataset):
+    # Get the job array index from SLURM
+    top =50
+    job_name = os.environ['SLURM_JOB_NAME']
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    print(f"Running job array index: {seed}")
+    # Set random seed for this job
+    fix_random_seed(seed)
 
-    labels_train = pd.read_csv(path + "/labels_tr.csv", header=None) 
-    labels_test = pd.read_csv(path + "/labels_te.csv", header=None)
-    labels = pd.concat([labels_train, labels_test], ignore_index=True)
-        
-    # omic 1 (mRNA)
-    data_1_train = pd.read_csv(path + "/1_tr.csv", header=None)
-    data_1_test = pd.read_csv(path + "/1_te.csv", header=None)
-    omic_1 = pd.concat([data_1_train, data_1_test], ignore_index=True)
-        
-    # omic 2 (DNA methylation)
-    data_2_train = pd.read_csv(path + "/2_tr.csv", header=None)
-    data_2_test = pd.read_csv(path + "/2_te.csv", header=None)
-    omic_2 = pd.concat([data_2_train, data_2_test], ignore_index=True)
+    folder = dataset 
+    # read in
+    comcat_omics = pd.read_csv(f'/nesi/nobackup/uoa03056/DL_prediction/Models/sim/sim_data/{folder}/combined_omics_{seed}.csv')
 
-    # omic 3 (miRNA)
-    data_3_train = pd.read_csv(path + "/3_tr.csv", header=None)
-    data_3_test = pd.read_csv(path + "/3_te.csv", header=None)
-    omic_3 = pd.concat([data_3_train, data_3_test], ignore_index=True)
+    x1_selected = comcat_omics[comcat_omics['omic_index'] == 1].values[:,:-2].T
+    x2_selected = comcat_omics[comcat_omics['omic_index'] == 2].values[:,:-2].T
+    x3_selected = comcat_omics[comcat_omics['omic_index'] == 3].values[:,:-2].T
 
-    return omic_1, omic_2, omic_3, labels
+    merged_matrix = np.hstack((x1_selected,x2_selected,x3_selected))
 
-def load_data(seed, kernel_par, n_principal):
-    fix_random_seed(seed=seed)
+    gene_index = comcat_omics['gene_index']
 
-    omic1, omic2, omic3, labels = import_dataset() 
-    y = labels.values.astype(np.int64)
-    indices = np.arange(len(y))
+    # concat them and set omics as gene i
+    gene_len = np.unique(gene_index).shape[0]
+
+    # the gene_group is a list of group size
+    grouped_features = []
+    gene_groups = []
+
+    unique_genes = np.unique(gene_index)
+
+    for gene in unique_genes:
+        gene_indices = np.where(gene_index == gene)[0] 
+        grouped_features.append(merged_matrix[:, gene_indices]) 
+        gene_groups.append(len(gene_indices))  
+
+    input_data = np.concatenate(grouped_features, axis=1)
+    labels = np.load(f'/nesi/nobackup/uoa03056/DL_prediction/Models/sim/sim_data/{folder}/{job_name}_to_binary_outcome_{seed}.npy')
+    #labels = np.load(f'/nesi/nobackup/uoa03056/DL_prediction/Models/sim/sim_data/Scenario2_Y/{folder}/{job_name}_to_contibuous_outcome_{seed}.npy')
+    #labels = np.load(f'/nesi/nobackup/uoa03056/DL_prediction/Models/sim/sim_data/Scenario3_Y/{folder}/{job_name}_to_contibuous_outcome_{seed}.npy')
+    labels= labels.astype(np.float32)
+    
+    indices = np.arange(len(labels))
+
+    # train test split
+    train_indices, test_indices = train_test_split(indices, test_size=0.2, random_state=seed)
+    
+    X_tr = input_data[train_indices]
+    X_te = input_data[test_indices]
+    
+    y_tr = labels[train_indices]
+    y_te = labels[test_indices]
+    
+    # scale
+    scaler_X = StandardScaler()
+    scaler_y = StandardScaler()
+    
+    #y_tr = scaler_y.fit_transform(y_tr.reshape(-1, 1))
+    #y_te = scaler_y.transform(y_te.reshape(-1, 1))
+    
+    X_tr= scaler_X.fit_transform(X_tr)
+    X_te = scaler_X.transform(X_te)
     
     
-    train_indices, test_indices = train_test_split(indices, test_size=0.3, random_state=seed, stratify=y)
-    y_tr = y[train_indices]
-    y_te = y[test_indices]
-    #print(train_indices)
+    # Convert to PyTorch tensors
+    X_tr = torch.tensor(X_tr, dtype=torch.float32)
+    X_te = torch.tensor(X_te, dtype=torch.float32)
+    
+    y_tr = torch.tensor(y_tr, dtype=torch.float32)
+    y_te = torch.tensor(y_te, dtype=torch.float32)
+    
 
-    #omic1 (mRNA) dataset
-    X_1 = omic1.values.astype(np.float64)
-    X_1_tr = X_1[train_indices]
-    K_PCA_1 = KernelPCA(n_components = n_principal, kernel='rbf', gamma=kernel_par[0], random_state=seed)
-    X_1_tr = K_PCA_1.fit_transform(X_1_tr)
-    X_1_te = X_1[test_indices]
-    X_1_te = K_PCA_1.transform(X_1_te)
-
-    train_1 = SingleOmicDataset(data=(X_1_tr, y_tr))
-    test_1  = SingleOmicDataset(data=(X_1_te, y_te))
-
-    #omic2 (mRNA)  dataset
-    X_2 = omic2.values.astype(np.float64)
-    X_2_tr = X_2[train_indices]
-    K_PCA_2 = KernelPCA(n_components = n_principal, kernel='rbf', gamma=kernel_par[1], random_state=seed)
-    X_2_tr = K_PCA_2.fit_transform(X_2_tr)
-    X_2_te = X_2[test_indices]
-    X_2_te = K_PCA_2.transform(X_2_te)
-
-    train_2 = SingleOmicDataset(data=(X_2_tr, y_tr))
-    test_2  = SingleOmicDataset(data=(X_2_te, y_te))
-
-    #omic3 (mRNA) dataset
-    X_3 = omic3.values.astype(np.float64)
-    X_3_tr = X_3[train_indices]
-    K_PCA_3 = KernelPCA(n_components = n_principal, kernel='rbf', gamma=kernel_par[2], random_state=seed)
-    X_3_tr = K_PCA_3.fit_transform(X_3_tr)
-    X_3_te = X_3[test_indices]
-    X_3_te = K_PCA_3.transform(X_3_te)
-
-    train_3 = SingleOmicDataset(data=(X_3_tr, y_tr))
-    test_3  = SingleOmicDataset(data=(X_3_te, y_te))
-
-    return [train_1, test_1], [train_2, test_2], [train_3, test_3]
+    return X_tr, X_te, y_tr, y_te, gene_groups
 
 
 if __name__ == "__main__":
